20bdf0694d5acc6e08f76214e631309af65eec24
unknown <hh54188@9AC08D9B9A65960.internal.baidu.com>
2015-07-10 10:34:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Math.js API
+
+http://mathjs.org/docs/reference/functions/alphabetical.html
+
+## Perlin Noise API
+
+https://github.com/josephg/noisejs
+
+## Vector.js API
+
+http://victorjs.org/
---
+++ js/victor-util.js
@@ -0,0 +1,39 @@
+if (!Victor) {
+}
+
+;(function (proto) {
+
+	/**
+	 * X与Y同时放大N倍
+	 * @param  {[Number]} times [放大的倍数]
+	 */
+	proto.multi = function (times) {
+		this.x = this.x * times;
+		this.y = this.y * times;
+		return this;
+	};
+
+	/**
+	 * X与Y同时除以N
+	 * @param  {[Number]} num [被除数]
+	 */
+	proto.div = function (num) {
+		this.x = this.x / num;
+		this.y = this.y / num;
+		return this;
+	};
+
+	/**
+	 * 两个矢量相减，原矢量不变，返回差为新值
+	 * @param  {[Object/Victor]} target [被减数]
+	 * @return {[Object/Victor]}        [差]
+	 */
+	proto.sub = function (target) {
+		return this.clone().subtract(target);
+	};
+
+	proto.limit = function () {
+		
+	}
+
+})(Victor.prototype);
