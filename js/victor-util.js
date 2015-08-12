b0cebe3d9f86c42821e20787d7041ba7d61e2ba6
hh54188 <252438823@qq.com>
2015-08-12 20:21:17 +0800

    update

--- js/victor-util.js
+++ js/victor-util.js
@@ -32,22 +32,8 @@ if (!Victor) {
 		return this.clone().subtract(target);
 	};
 
-	proto.addWithLimit = function (targetVictor) {
-		var testVictor = this.clone().add(targetVictor);
-		if (this.limitLength) {
-			testVictor.length() <= this.limitLength;
-			this.add(targetVictor);
-			return this;
-		} 
-		// 不存在limit或者超出limit
-		else {
-			return this;
-		}
-	};
-
-	proto.limitWithLength = function (targetLength) {
-		this.limitLength = targetLength;
-		return false;
+	proto.ad = function (target) {
+		return this.clone().add(target);
 	}
 
 })(Victor.prototype);
